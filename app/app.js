5331ef2c08740c118acc0c05f1a1df9555ed13e1
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-11-05 01:08:48 +0530

    Initial setup added

---
+++ app/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('app',[])
+        .controller('myctrl',function($scope){
+            $scope.message = "Welcome to Angular 1.6 !!!";
+        });
+
+        app.config(function(){
+
+        });
